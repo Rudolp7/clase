7b8e22d3a642cbb68ba0e7034de3cc25af16263b
Feliche <felichefie@gmail.com>
2022-09-26 11:30:43 -0500

    canvas

--- swing01.java
+++ swing01.java
@@ -1,19 +1,16 @@
 import javax.swing.*;
-import java.awt.Color;
 
 class swing01 {
     public static void main (String args[]){
         JFrame window = new JFrame("Swing");
-        JLabel label = new JLabel("hola Java swing");
-        JButton button = new JButton("boton");
-        Color color = new Color(255,0,0);
+        MyCanvas canvas = new MyCanvas();
 
-        window.setBackground(Color.GRAY);
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        button.setBounds(50, 50, 100, 40);
-        window.add(button);
-        window.add(label);
         window.setSize(400, 300);
+        window.add(canvas);
+        window.pack();
+        window.setResizable(false);
+        window.setLocationRelativeTo(null);
         window.setVisible(true);
     }
 }
